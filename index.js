4a735299132ad83b619fa00ed8d71dffd3a9d1dd
Krzysztof Antczak <k.antczak@inventic.it>
2012-09-02 16:07:49 +0200

    Improving pagination

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/** File Name: node_modules/railway-pagination/index.js
-* Purpose: railway-pagination main file.  
-* Original author: Anatoliy C.
-*
-* Update History
-* Name            Date       Description
-* --------------- ---------- ------------------------------------------------------------------------------
-* Jude L.         04/26/2012 - Updated the paginateCollection to allow the passing of order option to the Model.all routine.
-* Jude L.         05/19/2012 - Updated the paginateCollection to allow the passing of where option to the Model.all routine
-                              if one is provided.
-**/
-
 exports.init = function () {
     // add view helper
     railway.helpers.HelperSet.prototype.paginate = paginateHelper;
@@ -19,7 +7,8 @@ exports.init = function () {
 };
 
 // global view helper
-function paginateHelper(collection) {
+function paginateHelper(collection,step) {
+    if (!step) step = 5;
     if (!collection.totalPages || collection.totalPages < 2) return '';
     var page = parseInt(collection.currentPage, 10);
     var pages = collection.totalPages;
@@ -27,9 +16,29 @@ function paginateHelper(collection) {
     var prevClass = 'prev' + (page === 1 ? ' disabled': '');
     var nextClass = 'next' + (page === pages ? ' disabled': '');
     html += '<ul><li class="' + prevClass + '">';
+    html += railway.helpers.link_to('&larr; First', '?page=1');
     html += railway.helpers.link_to('&larr; Previous', '?page=' + (page - 1));
     html += '</li>';
-    for (var i = 1; i <= pages; i++ ) {
+
+    var start = ( page <= step ) ? 1 : page-step;
+    var end   = page+step;
+
+    if ( page > pages-step )
+    {
+        start = pages-(step*2);
+    }
+
+    if ( end < (step*2) )
+    {
+        end = step*2;
+    }
+
+    if ( end > pages )
+    {
+        end = pages;
+    }
+
+    for (var i = start; i <= end; i++ ) {
         if (i == page) {
             html += '<li class="active"><a href="#">' + i + '</a></li>';
         } else {
@@ -38,6 +47,7 @@ function paginateHelper(collection) {
     }
     html += '<li class="' + nextClass + '">';
     html += railway.helpers.link_to('Next &rarr;', '?page=' + (page + 1));
+    html += railway.helpers.link_to('Last &rarr;', '?page=' + pages);
     html += '</li></ul></div>';
     return html;
 };
@@ -45,21 +55,11 @@ function paginateHelper(collection) {
 // orm method
 function paginateCollection(opts, callback) {
     var limit = opts.limit || 10;
-    var page = opts.page || 1;
-    var order = opts.order||'1';
-    var where = opts.where;
+    var page  = opts.page || 1;
+    var order = opts.order||'1';  // added this line
     var Model = this;
 
     Model.count(function (err, totalRecords) {
-        if (where != null) {
-            Model.all({limit: limit, offset: (page - 1) * limit, order: order, where: where }, function (err, records) {
-                if (err) return callback(err);
-                records.totalRecords = totalRecords;
-                records.currentPage = page;
-                records.totalPages = Math.ceil(totalRecords / limit);
-                callback(null, records);
-            });
-        } else {
         Model.all({limit: limit, offset: (page - 1) * limit, order: order }, function (err, records) {
             if (err) return callback(err);
             records.totalRecords = totalRecords;
@@ -67,7 +67,5 @@ function paginateCollection(opts, callback) {
             records.totalPages = Math.ceil(totalRecords / limit);
             callback(null, records);
         });
-      }
     });
 }
-
